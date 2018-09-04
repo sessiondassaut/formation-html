4c108a6d17f7813217fb17f4591e4e982d3389a3
Jean-Baptiste NGUYEN <nguyen.jeanbaptiste45@gmail.com>
2018-09-04 10:01:02 +0200

    Ajout de index.php

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+
+<html>
+    <head>
+        <title>La Florentina</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+
+
+    <body>
+        <h1>La Florentina</h1>
+
+        <h2>Pizzéria</h2>
+
+        <nav>
+            <ul>
+                <li><a href="accueil.html">Accueil</a></li>
+                <li><a href="listepizza.html">Nos pizzas</a></li>
+                <li><a href="listeclients.html">Liste Clients</a></li>
+                <li><a href="liste.html">Liste</a></li>
+                <li><a href="tableau.html">Tableau</a></li>
+            </ul>
+
+        </nav>
+</html>
